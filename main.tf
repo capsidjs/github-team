d8c8c6fb6ab5613718a3b56cc3f5301e31970569
Yoshiya Hinosawa <stibium121@gmail.com>
2019-04-15 19:29:57 +0900

    fmt

--- main.tf
+++ main.tf
@@ -1,54 +1,56 @@
 variable "github_token" {
-	type = "string"
+  type = "string"
 }
 
 variable "admins" {
-	type = "list"
-	default = [
-		"kt3k"
-	]
+  type = "list"
+
+  default = [
+    "kt3k",
+  ]
 }
 
 variable "members" {
-	type = "list"
-	default = [
-		"hinosawa"
-	]
+  type = "list"
+
+  default = [
+    "hinosawa",
+  ]
 }
 
 provider "github" {
-	token = "${var.github_token}"
-	organization = "capsidjs"
+  token        = "${var.github_token}"
+  organization = "capsidjs"
 }
 
 resource "github_team" "maintainers" {
-	name = "maintainers"
-	description = "Core maintainers"
-	privacy = "closed"
+  name        = "maintainers"
+  description = "Core maintainers"
+  privacy     = "closed"
 }
 
 resource "github_membership" "admin" {
-	count = "${length(var.admins)}"
-	username = "${element(var.admins, count.index)}"
-	role = "admin"
+  count    = "${length(var.admins)}"
+  username = "${element(var.admins, count.index)}"
+  role     = "admin"
 }
 
 resource "github_membership" "member" {
-	count = "${length(var.members)}"
-	username = "${element(var.members, count.index)}"
-	role = "member"
+  count    = "${length(var.members)}"
+  username = "${element(var.members, count.index)}"
+  role     = "member"
 }
 
 resource "github_team_membership" "maintainers" {
-	count = "${length(var.admins)}"
-	team_id = "${github_team.maintainers.id}"
-	username = "${element(var.admins, count.index)}"
-	role = "maintainer"
+  count    = "${length(var.admins)}"
+  team_id  = "${github_team.maintainers.id}"
+  username = "${element(var.admins, count.index)}"
+  role     = "maintainer"
 }
 
 resource "github_team_membership" "team_membership_hinosawa" {
-	count = "${length(var.members)}"
-	team_id = "${github_team.maintainers.id}"
-	username = "${element(var.members, count.index)}"
-	role = "member"
+  count    = "${length(var.members)}"
+  team_id  = "${github_team.maintainers.id}"
+  username = "${element(var.members, count.index)}"
+  role     = "member"
 }
